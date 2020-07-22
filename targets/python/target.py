888193780719418c22fd86b5413a5d23a7ea1e35
sebgoa <runseb@gmail.com>
2020-07-22 12:30:19 +0200

    Update targets/python/target.py
    Co-authored-by: Antoine Cotten <hello@acotten.com>

--- targets/python/target.py
+++ targets/python/target.py
@@ -51,7 +51,7 @@ def target():
             v1.Event()
             .SetContentType("application/json")
             .SetData(data)
-            .SetEventID(str(uuid.uuid1()))
+            .SetEventID(str(uuid.uuid4()))
             .SetSource("from_your_own_target")
             .SetSubject("your_event_subject")
             .SetEventTime(datetime.now(timezone.utc).astimezone().isoformat())
